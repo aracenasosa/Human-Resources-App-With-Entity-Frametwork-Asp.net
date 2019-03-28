dd509074901c60e1e93cbe294ccf051538c8565d
miusuario <carlosthefather2014@hotmail.com>
2019-03-28 17:18:01 -0400

    Segundo Commit

---
+++ Human Resources/Controllers/PermissionsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Human_Resources;
+
+namespace Human_Resources.Controllers
+{
+    public class PermissionsController : Controller
+    {
+        private Human_ResourcesEntities2 db = new Human_ResourcesEntities2();
+
+        // GET: Permissions
+        public ActionResult Index()
+        {
+            var permission = db.Permission.Include(p => p.Employee1);
+            return View(permission.ToList());
+        }
+
+		public ActionResult Filtrar(String Name)//Filtrado de todos Permisos por Nombre
+		{
+			var filt = from s in db.Permission select s;
+			if (!String.IsNullOrEmpty(Name))
+			{
+				filt = filt.Where(j => j.Employee1.Name.Contains(Name));
+			}
+
+			return View(filt);
+		}
+
+		// GET: Permissions/Details/5
+		public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permission permission = db.Permission.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permission);
+        }
+
+        // GET: Permissions/Create
+        public ActionResult Create()
+        {
+            ViewBag.Employee = new SelectList(db.Employee, "Id", "Name");
+            return View();
+        }
+
+        // POST: Permissions/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Employee,Desde,Hasta,Comentarios")] Permission permission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Permission.Add(permission);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Employee = new SelectList(db.Employee, "Id", "Name", permission.Employee);
+            return View(permission);
+        }
+
+        // GET: Permissions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permission permission = db.Permission.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Employee = new SelectList(db.Employee, "Id", "Name", permission.Employee);
+            return View(permission);
+        }
+
+        // POST: Permissions/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Employee,Desde,Hasta,Comentarios")] Permission permission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permission).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Employee = new SelectList(db.Employee, "Id", "Name", permission.Employee);
+            return View(permission);
+        }
+
+        // GET: Permissions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permission permission = db.Permission.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permission);
+        }
+
+        // POST: Permissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Permission permission = db.Permission.Find(id);
+            db.Permission.Remove(permission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
